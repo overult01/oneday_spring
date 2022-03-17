c2d38f6a50aa89bf3ae40a3d6a08250e43fd70e6
it_st it <overult01@gmail.com>
2022-03-17 22:00:05 +0900

    first

---
+++ createdb.sql
@@ -0,0 +1,17 @@
+# 게시판 만들기
+
+/*
+CREATE SCHEMA `oneday` ;
+*/
+
+/*
+CREATE TABLE `oneday`.`text_table` (
+  `idnew_table` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(100) NOT NULL,
+  `writer` VARCHAR(45) NOT NULL,
+  `contents` VARCHAR(300) NULL,
+  PRIMARY KEY (`idnew_table`));
+*/
+
+use oneday;
+select * from text_table;
